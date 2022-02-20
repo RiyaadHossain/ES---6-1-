1cb2b7d4195d581e5529fea7511ce033537ccf0e
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-20 21:27:39 +0600

    map, find, filter methods

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* ========== Destructiong of Object ========== */
+/* ========== Destructuriong of Object ========== */
 const riyad = {
     fullname: 'Riyad Hossain',
     age: 20,
@@ -17,12 +17,49 @@ const { fullname, profession } = riyad;
 
 const { forntend } = riyad.workWith.framework;
 
-console.log(fullname, 'is a', profession, '. He works with', forntend)
+// console.log(fullname, 'is a', profession, '. He works with', forntend)
 
 
-/* ========== Destructiong of Array ========== */
+/* ========== Destructuriong of Array ========== */
 const arry = ['First One', 'Second One', 'Third One', 'Forth One']
 const [a, b] = arry;
 /* If you set Extra array element than vairables - it'll take first two */
 
-console.log(`${a} and ${b}`) 
+// console.log(`${a} and ${b}`) 
+
+
+/* ========== Array.map() ========== */  /* Return the same Array which was input in the parameter doing some calculation with the elements of the array */ 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const double = numbers.map(x => x * 2)
+const thriple = numbers.map(x => x * 3)
+
+
+// console.log(double)
+// console.log(thriple)
+
+
+
+/* ============= Simple Array of Object ============= */
+const elements = [
+    {name: 'Mobile', price: 50000},
+    {name: 'Phone', price: 12000},
+    {name: 'Pen', price: 10},
+    {name: 'Oil', price: 200}
+]
+
+const productName = elements.map(x => x.name);
+
+ elements.forEach(x => console.log(x.price))  /* forEach Method */
+
+// console.log(productName)
+
+
+/* ========== Array.filter() ========== */  /* Return - an Array containg specific elements */  
+const expensiveItem = elements.filter(x => x.price > 10000)
+console.log(expensiveItem)
+
+
+/* ========== Array.find() ========== */ /* Return - an element */  
+const firstChoice = elements.find(x => x.price > 40)
+console.log(firstChoice)

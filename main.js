9c7a1d8c1cf61be9126232a3a57d0c782b828dbf
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-20 17:35:23 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Destructiong:
+const riyad = { 
+    fullname: 'Riyad Hossain',
+    age: 20,
+    profession: 'web Developer',
+    country: 'BD'
+}
+
+const { fullname, profession } = riyad;
+console.log(fullname, 'is a', profession)

c4aeff0872286d3622113baf8971807ae5d13a09
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-20 17:45:12 +0600

    object destructing

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-// Destructiong:
-const riyad = { 
+/* ========== Destructiong of Object ========== */
+const riyad = {
     fullname: 'Riyad Hossain',
     age: 20,
     profession: 'web Developer',
-    country: 'BD'
+    country: 'BD',
+    workWith: {
+        web: 'html',
+        interactivity: 'js',
+        framework: {
+            forntend: 'React'
+        }
+    }
 }
 
 const { fullname, profession } = riyad;
-console.log(fullname, 'is a', profession)
+
+
+const { forntend } = riyad.workWith.framework;
+
+console.log(fullname, 'is a', profession, '. He works with', forntend)
+
+

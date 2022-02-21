ceb58598c66c54a94ced1a8169b02712ad49c5d2
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-21 08:14:07 +0600

    Class Inheritance

--- main.js
+++ main.js
@@ -50,16 +50,73 @@ const elements = [
 
 const productName = elements.map(x => x.name);
 
- elements.forEach(x => console.log(x.price))  /* forEach Method */
+//  elements.forEach(x => console.log(x.price))  /* forEach Method - doesn't return anything */ 
 
 // console.log(productName)
 
 
 /* ========== Array.filter() ========== */  /* Return - an Array containg specific elements */  
 const expensiveItem = elements.filter(x => x.price > 10000)
-console.log(expensiveItem)
+// console.log(expensiveItem)
 
 
 /* ========== Array.find() ========== */ /* Return - an element */  
 const firstChoice = elements.find(x => x.price > 40)
-console.log(firstChoice)
+// console.log(firstChoice)
+
+
+
+/* ========== Class ========== */
+class Person{
+    firstName;
+    lastName;
+    age;
+    constructor(fName, lName, age) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.age = age;
+    }
+    fullName() {
+        return `${this.firstName} ${this.lastName}` 
+    }
+    fullDetails() {
+        return `${this.fullName()} is ${this.age} years Old. He is a ${this.job}`
+    }
+}
+
+const Riyad = new Person('Riyad', 'Hossain', 20)
+
+// console.log(Riyad)
+// console.log(Riyad.fullDetails())
+
+
+class Developer extends Person{
+    designation;
+    constructor(fName, lName, age, job) {
+        super(fName, lName, age)
+        this.designation = job
+    }
+    doIt(what) {
+        return `${this.fullName()} is a ${this.designation} who ${what}.`
+    }
+}
+
+const shuvo = new Developer('Kazi', 'Shuvo', 27, 'Developer')
+const shuvoJob = shuvo.doIt('codes') /* call the method using the dynamically created Object */
+
+class Designer extends Person{
+    designation;
+    constructor(fName, lName, age, job) {
+        super(fName, lName, age)
+        this.designation = job
+    }
+    doIt(what) {
+        return `${this.fullName()} is a ${this.designation} who ${what}.`
+    }
+}
+
+const sangram = new Designer('Sangram', 'Bhuiya', 19, 'Designer')
+const sangramJob = sangram.doIt('designs') /* call the method using the dynamically created Object */
+
+
+console.log(shuvoJob)
